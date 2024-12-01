1fd559ad7a6d49c958e16ab5f6d9e61e9a29bc2d
VigneshVk47 <87011509+VigneshVk47@users.noreply.github.com>
2024-12-01 08:14:50 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	int a=60, b=60, c=60;
+if(a+b+c==180){
+	System.out.println("Triangle can be formed");
+}else{
+ 	System.out.println("Triangle cannot be formed");   
+}
+	}
+}
