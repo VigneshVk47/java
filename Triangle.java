d5f65caa22bf11a2d7736669c2f4d6acf3c75b47
VigneshVk47 <87011509+VigneshVk47@users.noreply.github.com>
2024-12-01 08:58:50 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
 public class Main
 {
-	public static void main(String[] args) {
-	int a=60, b=60, c=60;
+public static void main(String[] args) {
+	    Scanner scan=new Scanner(System.in);
+	int a=scan.nextInt();
+int b=scan.nextInt();
+int c=scan.nextInt();
 if(a+b+c==180){
 	System.out.println("Triangle can be formed");
 }else{
  	System.out.println("Triangle cannot be formed");   
 }
-	}
+		}
 }
