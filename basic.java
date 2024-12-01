601eb583c588cdc86cd58b56f6b69d23356c5aea
VigneshVk47 <87011509+VigneshVk47@users.noreply.github.com>
2024-12-01 08:11:04 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	int a=2;
+	String str="Name";
+	char b='y';
+	System.out.println(a);
+	System.out.println(str);
+	System.out.println(b);
+	}
+}
