dab27083290a76e8a3e84e0c7266c3046bc7466d
VigneshVk47 <87011509+VigneshVk47@users.noreply.github.com>
2024-12-02 09:04:55 +0530

    Create string loop.java

---
+++ string loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+String s=scan.nextLine();
+String s2=scan.nextLine();
+int a=scan.nextInt();
+for(int i=0;i<a;i++){
+    System.out.println(s+""+s2);
+}
+	}
+}
