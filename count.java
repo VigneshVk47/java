bfbad8da210211241f62b40fac28bf9568ac9410
VigneshVk47 <87011509+VigneshVk47@users.noreply.github.com>
2024-12-17 07:54:55 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=9879;
+		int count=0;
+		while(n!=0){
+		    n=n/10;
+		    count++;
+		}
+			    System.out.println(count);
+	}
+}
